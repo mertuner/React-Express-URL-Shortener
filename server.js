254c5207b0aaa3b91f8f208494d986add6b6d5e8
mert uner <mertuner@merts-MacBook-Air.local>
2022-02-02 17:19:54 -0800

    homepage static react

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const cors = require('cors');
 const redirectRoutes = require('./routes/redirect');
 const handlingRoutes = require('./routes/hash')
 
+const path = require("path")
+
 const app = express();
 
 app.use(cors())
 
+
+console.log(path.join(__dirname, './client/build'))
 app.use(bodyParser.json({ limit: '50mb' }));
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static("client/build"));
-// }
+app.use(express.static(path.join(__dirname, './client/build')));
 
-app.use(express.static("client/build"));
 app.use(redirectRoutes);
 app.use(handlingRoutes);
 
